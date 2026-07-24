9813076d2127047fff4c866628986cdf6f6d0e43
Alexandre Lunelli da Silva <ovelhaaa@gmail.com>
2026-07-24 14:12:03 -0300

    feat(apollo): implement phase 2 UI layout

--- Apollo/Source/PluginEditor.h
+++ Apollo/Source/PluginEditor.h
@@ -1,45 +1,45 @@
 #pragma once
+
 #include <juce_audio_processors/juce_audio_processors.h>
-#include "PluginProcessor.h"
 #include "ApolloLookAndFeel.h"
 #include "PluginProcessor.h"
 
-class ApolloAudioProcessorEditor : public juce::AudioProcessorEditor
+class ApolloAudioProcessorEditor : public juce::AudioProcessorEditor,
+                                   private juce::Timer
 {
 public:
-    ApolloAudioProcessorEditor (ApolloAudioProcessor&);
+    explicit ApolloAudioProcessorEditor (ApolloAudioProcessor&);
     ~ApolloAudioProcessorEditor() override;
 
     void paint (juce::Graphics&) override;
     void resized() override;
 
 private:
+    void timerCallback() override;
+    void updateStatePresentation();
+    void updateValueLabels();
+
     ApolloAudioProcessor& audioProcessor;
-
-    // --- GUI Components ---
-    juce::GroupComponent groupMix;
-    juce::Slider faderMix;
-    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> faderMixAttachment;
-    juce::Label lblMix;
-
-    // Knobs
-    juce::Slider knobDecay, knobPredelay, knobDamp, knobModSpeed, knobModDepth, knobEq1, knobEq2;
-    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> attachDecay, attachPredelay, attachDamp, attachModSpeed, attachModDepth, attachEq1, attachEq2;
-    juce::Label lblDecay, lblPredelay, lblDamp, lblModSpeed, lblModDepth, lblEq1, lblEq2;
-
-    // ComboBoxes
-    juce::ComboBox comboTimeScale, comboEffectMode, comboFootswitchMode;
-    std::unique_ptr<juce::AudioProcessorValueTreeState::ComboBoxAttachment> attachTimeScale, attachEffectMode, attachFootswitchMode;
-    juce::Label lblTimeScale, lblEffectMode, lblFootswitchMode;
-
-    // Toggle Buttons
-    juce::ToggleButton btnInputDiffusion, btnOctaveDryMix, btnBypass, btnMomentaryEffect;
-    std::unique_ptr<juce::AudioProcessorValueTreeState::ButtonAttachment> attachInputDiffusion, attachOctaveDryMix, attachBypass, attachMomentaryEffect;
-
-    juce::DropShadowEffect dropShadowBypass;
-    juce::Label titleLabel;
-
     ApolloLookAndFeel customLookAndFeel;
+
+    juce::Label titleLabel, globalStateLabel, helpLabel;
+    juce::GroupComponent reverbGroup, octaveGroup, performanceGroup, outputGroup;
+    juce::Label octaveStateLabel, performanceStateLabel;
+
+    juce::Slider faderMix;
+    juce::Slider knobDecay, knobPredelay, knobDamp, knobModSpeed, knobModDepth, knobEq1, knobEq2;
+    juce::Label lblMix, lblDecay, lblPredelay, lblDamp, lblModSpeed, lblModDepth, lblEq1, lblEq2;
+    juce::Label valueMix, valueDecay, valuePredelay, valueDamp, valueModSpeed, valueModDepth, valueEq1, valueEq2;
+
+    juce::ComboBox comboTimeScale, comboEffectMode, comboFootswitchMode;
+    juce::Label lblTimeScale, lblEffectMode, lblFootswitchMode;
+    juce::ToggleButton btnInputDiffusion, btnOctaveDryMix, btnBypass, btnMomentaryEffect;
+    juce::Label dryLabel, wetLabel;
+
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> faderMixAttachment;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> attachDecay, attachPredelay, attachDamp, attachModSpeed, attachModDepth, attachEq1, attachEq2;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::ComboBoxAttachment> attachTimeScale, attachEffectMode, attachFootswitchMode;
+    std::unique_ptr<juce::AudioProcessorValueTreeState::ButtonAttachment> attachInputDiffusion, attachOctaveDryMix, attachBypass, attachMomentaryEffect;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ApolloAudioProcessorEditor)
 };
